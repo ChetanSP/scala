637f31355e93bdb13ea8bcbc721d68775298be8a
Sp <chetsp@deloitte.com>
2020-07-16 22:48:18 +0530

    Minor improvements to DatabaseReadWrite.

---
+++ DatabaseReadWrite/src/main/scala/databaseReadWrite/Test.scala
@@ -0,0 +1,25 @@
+package databaseReadWrite
+
+import databaseReadWrite.configProperties
+
+//val configProperties = new configProperties
+
+object Test extends App {
+
+  println(s" OracleURL is : ${configProperties.OracleURL}")
+  /*  println(s" OracleUsername is : ${configProperties.OracleUsername}")
+  println(s" OraclePassword is : ${configProperties.OraclePassword}")
+  println(s" OracleDriver is : ${configProperties.OracleDriver}")
+
+  println(s" PostGreSQLURL is : ${configProperties.PostGreSQLURL}")
+  println(s" PostGreSQLUsername is : ${configProperties.PostGreSQLUsername}")
+  println(s" PostGreSQLPassword is : ${configProperties.PostGreSQLPassword}")
+  println(s" PostGreSQLDriver is : ${configProperties.PostGreSQLDriver}")
+
+  println(s" MemSQLusername is : ${configProperties.MemSQLusername}")
+  println(s" MemSQLpassword is : ${configProperties.MemSQLpassword}")
+  println(s" MemSQLHost is : ${configProperties.MemSQLHost}")
+  println(s" MemSQLPort is : ${configProperties.MemSQLPort}")
+  println(s" MemSQLDatabase is : ${configProperties.MemSQLDatabase}")
+  println(s" MemSQLVerification is : ${configProperties.MemSQLVerification}")*/
+}
--- DatabaseReadWrite/src/main/scala/databaseReadWrite/configProperties.scala
+++ DatabaseReadWrite/src/main/scala/databaseReadWrite/configProperties.scala
@@ -9,5 +9,22 @@ object configProperties {
   connections.load(getClass().getResourceAsStream("/config.properties"))
   PropertyConfigurator.configure(connections)
 
-  val pass = connections.getProperty("pass")
+  val environment = connections.getProperty("environment")
+
+  val OracleURL = connections.getProperty("OracleURL")
+  val OracleUsername = connections.getProperty("OracleUsername")
+  val OraclePassword = connections.getProperty("OraclePassword")
+  val OracleDriver = connections.getProperty("OracleDriver")
+
+  val PostGreSQLURL = connections.getProperty("PostGreSQLURL")
+  val PostGreSQLUsername = connections.getProperty("PostGreSQLUsername")
+  val PostGreSQLPassword = connections.getProperty("PostGreSQLPassword")
+  val PostGreSQLDriver = connections.getProperty("PostGreSQLDriver")
+
+  val MemSQLusername = connections.getProperty("MemSQLusername")
+  val MemSQLpassword = connections.getProperty("MemSQLpassword")
+  val MemSQLHost = connections.getProperty("MemSQLHost")
+  val MemSQLPort = connections.getProperty("MemSQLPort")
+  val MemSQLDatabase = connections.getProperty("MemSQLDatabase")
+  val MemSQLVerification = connections.getProperty("MemSQLVerification")
 }
