64f43ce0a808570c9f0db4296188d18666ee5c85
chetsp <chetan170sp@gmail.com>
2021-04-04 01:23:09 +0530

    Modifying MergeSort Implementation.

--- DS/src/main/scala/MyAlgorithms/MergeSort.scala
+++ DS/src/main/scala/MyAlgorithms/MergeSort.scala
@@ -1,28 +1,26 @@
 package MyAlgorithms
 
-object MergeSort extends App{
+object MergeSort extends App {
 
-  // recursive merge of 2 sorted lists
-  def merge(left: List[Int], right: List[Int]): List[Int] =
+  // recursively merge 2 sorted lists
+  def merger(left: List[Int], right: List[Int]): List[Int] =
     (left, right) match {
       case (left, Nil) => left
       case (Nil, right) => right
       case (leftHead :: leftTail, rightHead :: rightTail) =>
-        if (leftHead < rightHead) leftHead :: merge(leftTail, right)
-        else rightHead :: merge(left, rightTail)
+        if (leftHead < rightHead) leftHead :: merger(leftTail, right)
+        else rightHead :: merger(left, rightTail)
     }
 
-  def mergeSort(list: List[Int]): List[Int] = {
+  def splitter(list: List[Int]): List[Int] = {
     val n = list.length / 2
     if (n == 0) list // i.e. if list is empty or single value, no sorting needed
     else {
       val (left, right) = list.splitAt(n)
-      merge(mergeSort(left), mergeSort(right))
+      merger(splitter(left), splitter(right))
     }
   }
 
-  println(s" Sorted data is: "+ mergeSort(List(33, 44, 22, -10, 99))) //> res0: List[Int] = List(-10, 22, 33, 44, 99)
+  println(s" Sorted data is: " + splitter(List(33, 44, 22, -10, 99))) //> res0: List[Int] = List(-10, 22, 33, 44, 99)
 
-
-  //mergeSort(List()) //> res1: List[Int] = List()
 }
